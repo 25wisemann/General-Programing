ebf54d8ab517b6736fa2b93c2cef3715fa2d8446
Wiseman <25WisemanN@badinhs.org>
2025-01-17 13:17:04 -0500

    created notes for data types

---
+++ VariableNotes.js
@@ -0,0 +1,81 @@
+// //this is comment
+// //we will use combo of coments and code to build notes
+
+// //Declaring a variable
+// let myName = "Jeff"
+// console.log(myName);
+// myName = "steve"
+// console.log(myName);
+
+// //Here is differnet way to declare a varriable
+// const e = 2.71828;
+// console.log(e);
+// //  e = 3.1415;
+// // console.log(e);
+// //^^^ Make the thing not good, const can not change
+
+// // a third way to de clare a variable
+// var pi = 3.1415;
+// console.log(pi);
+// pi = e
+// console.log(pi)
+
+// // Var and Let are nearly the same, interchangable
+// // Const is not changable
+
+// let total = 0;
+// let i = 0;
+// while (i < 1001){
+//     total = total + i;
+//     i = i + 1;
+// }
+// console.log(total)
+
+// //Independent math testing
+// let z = 5
+// let x = 5*z + z^2
+// console.log(x)
+
+// We'll start looking at number and math bellow
+// Java Script can add
+console.log(23+97);
+
+//You can store values in variables
+let SumOfSix = 5 + 3 + 98 + 2 + 69 + 96;
+console.log(SumOfSix);
+
+// You can do math in the console including variables from the Java script file you ran
+
+// Simulaton of calculatig percent error
+const actual = 57;
+let prediction = actual - 13;
+let percentage = prediction / actual;
+let PercentError = 1- percentage
+console.log(PercentError)
+
+/* Starts a multiline or block comment 
+It at the End, ends the comment*/
+
+/* Math terms in Java
++ -> Add
+- -> Subtract
+* -> multiply
+/ -> Divide
+** -> Exponet
+% -> Modulus 
+
+JavaScript will work like a sienctific calculator and apply order of opperations
+(Modulus with Multiply and Divide)
+
+Unlike Many other Prgraming languages, JavaScript only has one number type
+Other languages often have at least two others
+int- integers
+float- floating point numbers (decimals)
+*/
+
+/* There are some rules for variable names
+1. The Name may only contain letters, didgits, or the symbols: $ and _
+2. The first character may not be a didgit
+3. They may not be reserved words
+*/
+
