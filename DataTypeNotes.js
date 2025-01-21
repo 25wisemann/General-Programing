dddf4cef0c88c24748b5bd027e2507f9b9efb5eb
Wiseman <25WisemanN@badinhs.org>
2025-01-21 13:19:10 -0500

    notes

--- DataTypeNotes.js
+++ DataTypeNotes.js
@@ -1,1 +1,13 @@
 // These will be notes on data types
+
+// There are 8 data types in JavaScript
+
+/*
+1. Numbers (integers and Floating)
+    - In addition to traditional numbers this includes infinity and NAN (Not a Number)
+2. BigInt
+    - For very large numbers, Java Script starts to estimate numbers
+    - This can be overidden with a BigInt, formed by a lowercase n and the ebd of the number
+3. String
+    - Letters words, characters, numbers, symbols, that are treated as text
+*/
