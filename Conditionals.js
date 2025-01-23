09cf341e070cacfdb66d448cbd0d3ea0ee8f2358
Wiseman <25WisemanN@badinhs.org>
2025-01-23 13:19:24 -0500

    notes

--- Conditionals.js
+++ Conditionals.js
@@ -1,1 +1,31 @@
 // These are notes on conditionals in programing
+
+/* A conditioal is basicaly an if and then statement
+We will have more complicated examples, but all rely on the idea:
+    you are checking if a condition is true, then acting based on that condition
+They rely on some form of comparrison
+*/
+
+/*
+Comparrisons in JavaScript look like math equations to some extent
+a < b (is a less than b)
+a > b (is a grater than b)
+a == b (is a equal to b)
+a <= b (less than or equal)
+a >= b (grater tha or equal)
+*/
+
+// Examples:
+let a = 5;
+console.log(5 < 3);
+console.log(5 > 3);
+console.log(a == 5);
+console.log(a == "5");
+
+// JavaScript aslo has a tripple equal sign, for strick equality.
+// Meaning that is a equal to b and have the same data type.
+console.log(a === '5')
+console.log(a === 5)
+
+// the last symbol is "!"
+// "!" means not
