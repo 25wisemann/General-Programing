ed89c53ca9176b0045cfa582101abf91f0a1c3cf
Wiseman <25WisemanN@badinhs.org>
2025-02-07 11:18:59 -0500

    Homework

---
+++ FunctionsPractice.js
@@ -0,0 +1,31 @@
+//1
+function multiply(a, b){
+    console.log(a * b)
+}
+
+multiply(4, 7)
+
+//4
+
+function checkAge(age) { 
+    if (age > 18) { 
+         return true; 
+     } else { 
+         return confirm('Did parents allow you?');
+}
+}
+
+
+function checkAge(age) { 
+    if (age > 18) { 
+       return true;
+} 
+return confirm('Did parents allow you?');
+}
+
+checkAge(17)
+
+//They both return the same thing
+
+//5
+
