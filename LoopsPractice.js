2f59f3ce8e20ccc39244c5913c6ef0540c886227
Wiseman <25WisemanN@badinhs.org>
2025-02-21 13:43:40 -0500

    Got to problem 4

---
+++ LoopsPractice.js
@@ -0,0 +1,37 @@
+// Assignment Start
+// let Evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// for (let i = 0; i < Evens.length; i++){
+//     console.log(Evens[i])
+// }
+
+//1
+// for(let i = 1; i < 21; i++){
+//     if (i%2==0){console.log(i)}
+// }
+
+// 2
+// let i = 0
+// while (i <= 3){
+//     alert(`number ${i}`)
+//     i++
+// }
+
+//3
+
+// while(true){
+
+//     let value = +prompt('Enter a number', '')
+//     if (!value){
+//         break
+//     }
+//     if (value > 100){
+//         berak
+//     }
+// }
+
+//4
+while(true){
+    let value = [+prompt('Input number', '')]
+
+
+}
